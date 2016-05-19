bae7a29c5dcc7f1e5945b7de8a10a80ce73a971c
SamsetDev <singh.891sanjay@gmail.com>
2016-05-19 16:30:59 +0530

    GplusSocialIntegration
    GplusSocialIntegration

---
+++ GplusSocialIntegration/app/src/main/java/com/samset/gplussocialintegration/frafments/MainFragment.java
@@ -0,0 +1,133 @@
+package com.samset.gplussocialintegration.frafments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.github.gorbin.asne.core.SocialNetwork;
+import com.github.gorbin.asne.core.SocialNetworkManager;
+import com.github.gorbin.asne.core.listener.OnLoginCompleteListener;
+import com.github.gorbin.asne.googleplus.GooglePlusSocialNetwork;
+import com.samset.gplussocialintegration.MainActivity;
+import com.samset.gplussocialintegration.R;
+import com.samset.gplussocialintegration.frafments.ProfileFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainFragment extends Fragment implements SocialNetworkManager.OnInitializationCompleteListener, OnLoginCompleteListener {
+
+    public static SocialNetworkManager mSocialNetworkManager;
+    private Button googleplus;
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle(R.string.app_name);
+        // init buttons and set Listener
+
+        googleplus = (Button) rootView.findViewById(R.id.googleplus);
+        googleplus.setOnClickListener(loginClick);
+
+        //Use manager to manage SocialNetworks
+        mSocialNetworkManager = (SocialNetworkManager) getFragmentManager().findFragmentByTag(MainActivity.SOCIAL_NETWORK_TAG);
+
+        //Check if manager exist
+        if (mSocialNetworkManager == null) {
+            mSocialNetworkManager = new SocialNetworkManager();
+
+
+            //Init and add to manager LinkedInSocialNetwork
+            GooglePlusSocialNetwork gpNetwork = new GooglePlusSocialNetwork(this);
+            mSocialNetworkManager.addSocialNetwork(gpNetwork);
+
+            //Initiate every network from mSocialNetworkManager
+            getFragmentManager().beginTransaction().add(mSocialNetworkManager, MainActivity.SOCIAL_NETWORK_TAG).commit();
+            mSocialNetworkManager.setOnInitializationCompleteListener(this);
+        } else {
+            //if manager exist - get and setup login only for initialized SocialNetworks
+            if(!mSocialNetworkManager.getInitializedSocialNetworks().isEmpty()) {
+                List<SocialNetwork> socialNetworks = mSocialNetworkManager.getInitializedSocialNetworks();
+                for (SocialNetwork socialNetwork : socialNetworks) {
+                    socialNetwork.setOnLoginCompleteListener(this);
+                    initSocialNetwork(socialNetwork);
+                }
+            }
+        }
+        return rootView;
+    }
+
+    private void initSocialNetwork(SocialNetwork socialNetwork){
+        if(socialNetwork.isConnected()){
+            switch (socialNetwork.getID()){
+                case GooglePlusSocialNetwork.ID:
+                    googleplus.setText("Show GooglePlus profile");
+                    break;
+            }
+        }
+    }
+    @Override
+    public void onSocialNetworkManagerInitialized() {
+        //when init SocialNetworks - get and setup login only for initialized SocialNetworks
+        for (SocialNetwork socialNetwork : mSocialNetworkManager.getInitializedSocialNetworks()) {
+            socialNetwork.setOnLoginCompleteListener(this);
+            initSocialNetwork(socialNetwork);
+        }
+    }
+
+    //Login listener
+
+    private View.OnClickListener loginClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int networkId = 0;
+            switch (view.getId()){
+
+                case R.id.googleplus:
+                    networkId = GooglePlusSocialNetwork.ID;
+                    break;
+            }
+            SocialNetwork socialNetwork = mSocialNetworkManager.getSocialNetwork(networkId);
+            if(!socialNetwork.isConnected()) {
+                if(networkId != 0) {
+                    socialNetwork.requestLogin();
+                    MainActivity.showProgress("Loading social person");
+                } else {
+                    Toast.makeText(getActivity(), "Wrong networkId", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                startProfile(socialNetwork.getID());
+            }
+        }
+    };
+
+    @Override
+    public void onLoginSuccess(int networkId) {
+        MainActivity.hideProgress();
+        startProfile(networkId);
+    }
+
+    @Override
+    public void onError(int networkId, String requestID, String errorMessage, Object data) {
+        MainActivity.hideProgress();
+        Toast.makeText(getActivity(), "ERROR: " + errorMessage, Toast.LENGTH_LONG).show();
+    }
+
+    private void startProfile(int networkId){
+        ProfileFragment profile = ProfileFragment.newInstannce(networkId);
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .addToBackStack("profile")
+                .replace(R.id.container, profile)
+                .commit();
+    }
+}
